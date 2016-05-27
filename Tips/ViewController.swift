1c3cd67385bf4c8c9a469caae1c12a7bc765464b
Dang Quoc Huy <huysamdua@yahoo.com>
2016-05-27 22:36:22 +0700

    Rework UI + functions - add split bill function - add round function

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    var tipPercentages = [0.18, 0.2, 0.25]
+    @IBOutlet weak var splitToField: UITextField!
+    @IBOutlet weak var roundControl: UISegmentedControl!
+    @IBOutlet weak var totalTextLabel: UILabel!
+    @IBOutlet weak var tipTextLabel: UILabel!
+    
+    var tipPercentages = [0.05, 0.1, 0.15, 0.2, 0.25]
+    var numDecimal = 0
     
     // MARK: functions
     
@@ -25,7 +31,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
-        
+        billField.text = ""
+        splitToField.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,15 +45,39 @@ class ViewController: UIViewController {
     @IBAction func onEditingChanged(sender: AnyObject) {
         let billAmount = NSString(string: billField.text!).doubleValue
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        let tip = billAmount * tipPercentage
-        let total = billAmount + tip
+        let splitTo = NSString(string: splitToField.text!).doubleValue
+        let roundOption = roundControl.selectedSegmentIndex
+        var tip: Double
+        var total: Double
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        if splitTo > 1 {
+            totalTextLabel.text = "Each"
+            tipTextLabel.text = "Each tip"
+            tip = (billAmount * tipPercentage)/splitTo
+            total = (billAmount + tip)/splitTo
+        } else {
+            totalTextLabel.text = "Total"
+            tipTextLabel.text = "Tip"
+            tip = billAmount * tipPercentage
+            total = billAmount + tip
+        }
+        
+        switch roundOption {
+        case 0:
+            tip = ceil(tip)
+            total = ceil(total)
+        case 2:
+            tip = floor(tip)
+            total = floor(total)
+        default: break
+        }
+        
+        let numFormat = "$%.\(numDecimal)f"
+        tipLabel.text = String(format: numFormat, tip)
+        totalLabel.text = String(format: numFormat, total)
     }
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
 }
-
