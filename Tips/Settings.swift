9b5626c5d0fdab364807f6b0392b1b10069d1e8c
Dang Quoc Huy <huysamdua@yahoo.com>
2016-05-28 08:35:22 +0700

    add Settings page code

---
+++ Tips/Settings.swift
@@ -0,0 +1,19 @@
+//
+//  Settings.swift
+//  Tips
+//
+//  Created by Dang Quoc Huy on 5/28/16.
+//  Copyright © 2016 Dang Quoc Huy. All rights reserved.
+//
+
+import Foundation
+
+struct SettingKeys {
+    static let satisfactionKey = "satisfactionKey"
+    static let splitToKey = "splitToKey"
+    static let roundOptKey = "roundOptKey"
+    static let currencySymbolKey = "currencySymbolKey"
+    static let numOfDecimalKey = "numOfDecimalKey"
+    static let languageKey = "languageKey"
+    static let themeColorKey = "themeColorKey"
+}
--- Tips/SettingsViewController.swift
+++ Tips/SettingsViewController.swift
@@ -9,13 +9,53 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    // MARK: properties
+    
+    @IBOutlet weak var satisfactionControl: UISegmentedControl!
+    @IBOutlet weak var splitToField: UITextField!
+    @IBOutlet weak var roundOptionControl: UISegmentedControl!
+    @IBOutlet weak var currencySymbolControl: UISegmentedControl!
+    @IBOutlet weak var numOfDecimalControl: UISegmentedControl!
+    @IBOutlet weak var languageControl: UISegmentedControl!
+    @IBOutlet weak var themeControl: UISegmentedControl!
 
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    // MARK: functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // load settings
+        loadSettings()
     }
 
+    func loadSettings() {
+        satisfactionControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.satisfactionKey)
+        if let splitToText = defaults.objectForKey(SettingKeys.splitToKey) as! String? {
+            splitToField.text = splitToText
+        } else {
+            splitToField.text = ""
+        }
+        roundOptionControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.roundOptKey)
+        currencySymbolControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.currencySymbolKey)
+        numOfDecimalControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.numOfDecimalKey)
+        languageControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.languageKey)
+        themeControl.selectedSegmentIndex = defaults.integerForKey(SettingKeys.themeColorKey)
+    }
+    
+    func saveSettings() {
+        defaults.setInteger(satisfactionControl.selectedSegmentIndex, forKey: SettingKeys.satisfactionKey)
+        defaults.setObject(splitToField.text, forKey: SettingKeys.splitToKey)
+        defaults.setInteger(roundOptionControl.selectedSegmentIndex, forKey: SettingKeys.roundOptKey)
+        defaults.setInteger(currencySymbolControl.selectedSegmentIndex, forKey: SettingKeys.currencySymbolKey)
+        defaults.setInteger(numOfDecimalControl.selectedSegmentIndex, forKey: SettingKeys.numOfDecimalKey)
+        defaults.setInteger(languageControl.selectedSegmentIndex, forKey: SettingKeys.languageKey)
+        defaults.setInteger(themeControl.selectedSegmentIndex, forKey: SettingKeys.themeColorKey)
+        defaults.synchronize()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,6 +72,17 @@ class SettingsViewController: UIViewController {
     }
     */
 
+    // MARK: actions
+    
+    @IBAction func onValueChanged(sender: AnyObject) {
+        saveSettings()
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        defaults.setObject(splitToField.text, forKey: SettingKeys.splitToKey)
+        defaults.synchronize()
+    }
+    
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
